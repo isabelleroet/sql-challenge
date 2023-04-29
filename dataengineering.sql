853ac745e229c44627238d9530b4b1b21078a032
Isabelle <isabelleroet@gmail.com>
2023-04-29 10:29:07 -0700

    Fourth commit

--- dataengineering.sql
+++ dataengineering.sql
@@ -19,16 +19,24 @@
 );
 
 	CREATE TABLE "dept_emp" (
-		emp_no INT NOT NULL REFERENCES employees(emp_no),
-		dept_no VARCHAR NOT NULL REFERENCES departments(dept_no)
+    	dept_no VARCHAR NOT NULL,
+    	emp_no INT NOT NULL,
+    	PRIMARY KEY (dept_no, emp_no),
+    	FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
+    	FOREIGN KEY (dept_no) REFERENCES departments (dept_no)   
 );
-
 	CREATE TABLE "dept_manager" (
 		dept_no VARCHAR NOT NULL REFERENCES departments(dept_no),
-		emp_no INT NOT NULL REFERENCES employees(emp_no)
+		emp_no INT NOT NULL REFERENCES employees(emp_no),
+		PRIMARY KEY (dept_no, emp_no),
+		FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
+		FOREIGN KEY (dept_no) REFERENCES departments (dept_no)   
 );
 
 	CREATE TABLE "salaries" (
-		emp_no INT REFERENCES employees(emp_no),
-		salary INT 
+    	emp_no INT NOT NULL,
+    	salary INT NOT NULL,
+    	FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
+		PRIMARY KEY (emp_no)
+);
 );
